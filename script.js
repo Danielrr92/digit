bd67117318e4843cecc8f6b46e67e40fd78a2acf
Daniele Sanna <ds.percussioni@gmail.com>
2024-06-15 02:52:56 +0200

    adjustaments

--- script.js
+++ script.js
@@ -29,24 +29,20 @@ setText();
 
 
 function checkLetteraDigitata(e) {
-    let prossimaLettera = testoCompleto.substring(testoDigitato.length, testoDigitato.length + 1);;
-    // if (testoDigitato == "") {
-    //     prossimaLettera = testoCompleto.substring(0, 1)
-    // } else {
-    // prossimaLettera = testoCompleto.substring(testoDigitato.length, testoDigitato.length + 1);
-    // }
-
+    let prossimaLettera = testoCompleto.substring(testoDigitato.length, testoDigitato.length + 1);
+    
     if (e.key == prossimaLettera) {
         testoDigitato += e.key;
         let nuovoTesto = testoCompleto.substring(testoDigitato.length, testoDigitato.length + 30);
         let nuovoTestoMancante = testoCompleto.substring(testoDigitato.length + 1 + 30);
         document.getElementById('testo').innerHTML = nuovoTesto;
         document.getElementById('testoMancante').innerHTML = nuovoTestoMancante;
-        console.log("ho premuto la giusta lettera;")
     } else {
         totaleErrori++;
     }
+    
     if (testoDigitato.length == testoCompleto.length) {
         document.getElementById('errori').innerHTML = 'Hai commesso un totale di: ' + totaleErrori + ' errori.';
+        document.removeEventListener('keydown')
     }
 }
